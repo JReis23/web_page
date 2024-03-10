81f6af16a6eab2e76ef1648a577d209e112bc9b8
JReis23 <cjfreis23@gmail.com>
2024-03-10 20:26:07 +0100

    minors changes

--- src/lib/formValidations/formValidator.ts
+++ src/lib/formValidations/formValidator.ts
@@ -1,8 +1,38 @@
 import { z } from 'zod';
+import FR from '$lib/texts/FR.json';
+import EN from '$lib/texts/EN.json';
 
-// See https://zod.dev/?id=primitives for schema syntax
+const defaulLang = navigator.language.slice(0, 2);
+defaulLang.toUpperCase();
+interface ValidatorMessage {
+	form: {
+		nameRequired: string;
+		nameMinLength: string;
+		emailRequired: string;
+		emailInvalid: string;
+		messageRequired: string;
+		messageMinLength: string;
+		messageMaxLength: string;
+	};
+}
+
+const formValidator: ValidatorMessage[] = defaulLang === 'fr' ? FR.message : EN.message;
+
 export const userSchema = z.object({
-	name: z.string().min(2),
-	email: z.string().email(),
-	message: z.string().min(10)
+	name: z
+		.string({ required_error: formValidator[0].form.nameRequired })
+		.min(2, { message: formValidator[0].form.nameMinLength }),
+	email: z
+		.string({ required_error: formValidator[0].form.emailRequired })
+		.email({ message: formValidator[0].form.emailInvalid })
+		.regex(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z0-9-]+\.)*[a-zA-Z]{2,}))$/
+		),
+	message: z
+		.string({
+			required_error: formValidator[0].form.messageRequired
+		})
+		.min(5, { message: formValidator[0].form.messageMinLength })
+		.regex(/^[a-zA-Z0-9\s.,!?()'-]*$/)
+		.max(255, { message: formValidator[0].form.messageMaxLength })
 });
--- src/lib/stores/lang.ts
+++ src/lib/stores/lang.ts
@@ -3,4 +3,6 @@ import { writable } from 'svelte/store';
 const defaulLang = navigator.language.slice(0, 2);
 defaulLang.toUpperCase();
 
+console.log({ defaulLang });
+
 export const lang = writable(defaulLang);
